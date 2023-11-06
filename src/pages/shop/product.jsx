6da3987ed65b147afe1ceb52f93eaf87b31fa6cd
jimvang8790 <vangjim15@gmail.com>
2023-11-05 18:53:55 -0600

    cart component adding items

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
-const Product = (props) => {
-    const {id, productName, price, productImage} = props.data;
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemCount = cartItems[id];
+
   return (
-    <div className='product'>
-      <img src={productImage} alt=''/>
-      <div className='description'>
+    <div className="product">
+      <img src={productImage} />
+      <div className="description">
         <p>
           <b>{productName}</b>
         </p>
-        <p>${price}</p>
+        <p> ${price}</p>
       </div>
-      <button className='addToCartBttn'>Add To Cart</button>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Product
