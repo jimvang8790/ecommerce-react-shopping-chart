a37d2ccfb68b7b8ebab02656b3f49c20fee8cdf7
jimvang8790 <vangjim15@gmail.com>
2023-11-05 18:32:28 -0600

    setting cart item id

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from 'react'
+import { PRODUCTS } from '../products';
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+    let cart = {}
+    for (let i = 1; PRODUCTS.length + 1; i++) {
+        cart[i] = 0
+    }
+    return cart;
+}
+
 const ShopContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
+    }
     return (
     <ShopContext.Provider>{props.children}</ShopContext.Provider>
   )
