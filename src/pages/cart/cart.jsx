6713e0104835d42559b92865061878dd66624120
jimvang8790 <vangjim15@gmail.com>
2023-11-05 20:03:48 -0600

    adding subtotal to cart page

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,9 +3,12 @@ import { PRODUCTS } from '../../products'
 import { ShopContext } from '../../context/shop-context'
 import CartItem from './cart-item'
 import "./cart.css"; 
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
   return (
     <div className='cart'>
       <div><h1>Your Cart Items</h1></div>
@@ -16,6 +19,14 @@ const Cart = () => {
           }
         })}
       </div>
+      {totalAmount > 0 ?
+        <div className='checkout'>
+          
+          <p>Subtotal: ${totalAmount}</p>
+          <button onClick={() => navigate('/')}> Continue Shopping </button>
+          <button> Checkout </button>
+        </div>
+      : <h1>Your cart is empty</h1>}
     </div>
   )
 }
